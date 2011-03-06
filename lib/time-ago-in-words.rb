5881000b69199669a247d3387762100765cc1018
Simon H. Eskildsen <sirup@sirupsen.com>
2011-03-06 11:43:34 +0100

    Refactored core

--- lib/time-ago-in-words.rb
+++ lib/time-ago-in-words.rb
@@ -1,53 +1,52 @@
 require 'time'
+module Units
+  Second     = 1
+  Minute     = Second  * 60
+  Hour       = Minute  * 60
+  Day        = Hour    * 24
+  Week       = Day     * 7
+  Month      = Week    * 4
+  Year       = Day     * 365
+  Decade     = Year    * 10
+  Century    = Decade  * 10
+  Millennium = Century * 10
+end
+
 class Time
   def time_ago_in_words
-    time = self
-    time_now = Time.now.to_i
-    time_then = time.to_i
-    time_between = time_now - time_then
-    a_second     = 1
-    a_minute     = a_second  * 60
-    a_hour       = a_minute  * 60
-    a_day        = a_hour    * 24
-    a_week       = a_day     * 7
-    a_fortnight  = a_day     * 14
-    a_month      = a_week    * 4
-    a_quarter    = a_month   * 3
-    a_year       = a_month   * 24
-    a_olympiad   = a_year    * 4
-    a_lustrum    = a_year    * 5
-    a_decade     = a_lustrum * 2
-    a_indiction  = a_lustrum * 3
-    a_jubilee    = a_decade  * 5
-    a_century    = a_jubilee * 2
-    a_millennium = a_jubilee * 10
-    if time_between <= a_minute
-      unit = "second#{'s' unless time_between == a_second}"
-      "#{time_between} #{unit} ago"
-    elsif time_between <= a_hour
-      unit = "minute#{'s' unless time_between == a_minute}"
-      "#{time_between / a_minute} #{unit} ago"
-    elsif time_between <= a_day
-      unit = "hour#{'s' unless time_between == a_hour}"
-      "#{time_between / a_hour} #{unit} ago"
-    elsif time_between <= a_week
-      unit = "day#{'s' unless time_between == a_day}"
-      "#{time_between / a_day} #{unit} ago"
-    elsif time_between <= a_month
-      unit = "week#{'s' unless time_between == a_week}"
-      "#{time_between / a_week} #{unit} ago"
-    elsif time_between <= a_year
-      unit = "month#{'s' unless time_between == a_month}"
-      "#{time_between / a_month} #{unit} ago"
-    elsif time_between <= a_decade
-      unit = "year#{'s' unless time_between == a_year}"
-      "#{time_between / a_year} #{unit} ago"
-    elsif time_between <= a_century
-      unit = "decade#{'s' unless time_between == a_decade}"
-      "#{time_between / a_decade} #{unit} ago"
-    else
-      unit = "millennium#{'s' unless time_between == a_millennium}"
-      "#{time_between / a_millennium} #{unit} ago"
+    time_difference = Time.now.to_i - self.to_i
+    unit = get_unit(time_difference)
+    unit_difference = time_difference / Units.const_get(unit.capitalize)
+
+    unit = unit.to_s + ('s' if plural?(time_difference))
+
+    "#{unit_difference} #{unit} ago"
+  end
+
+  private
+  def get_unit(time_difference)
+
+    case time_difference
+    when 0...Minute
+      :second
+    when Minute...Hour
+      :minute
+    when Hour...Day
+      :hour
+    when Day...Week
+      :day
+    when Week...Month
+      :week
+    when Month...Year
+      :month
+    when Year...Decade
+      :year
+    when Decade..Century
+      :decade
     end
+  end
+
+  def plural?(unit_difference)
+    unit_difference > 1
   end
 end
