9bdbdf834ace57d7d136189d9d5da076a62a12a1
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2011-03-16 02:49:29 -0700

    Minor fix to namespace
    Signed-off-by: Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>

--- test/helper.rb
+++ test/helper.rb
@@ -1,3 +1,3 @@
 require 'minitest/spec'
 require 'minitest/autorun'
-require File.expand_path(File.dirname(__FILE__) + '/../lib/time-ago-in-words.rb')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/time-lord.rb')
